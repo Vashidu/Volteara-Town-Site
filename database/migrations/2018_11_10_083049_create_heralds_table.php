93419dcc55383f4508166a9123d6ba5851ae6a9e
vashidu <sopleb@bot.rip>
2018-11-10 08:31:16 +0000

    replace migration

--- database/migrations/2018_11_10_083049_create_heralds_table.php
+++ database/migrations/2018_11_10_083049_create_heralds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Heralds extends Migration
+class CreateHeraldsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class Heralds extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('heralds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class Heralds extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('heralds');
     }
 }
