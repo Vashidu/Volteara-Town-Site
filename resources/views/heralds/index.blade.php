5481ac7b2aaacf17154d3d08b64b288d4a058bee
vashidu <sopleb@bot.rip>
2018-11-10 08:25:45 +0000

    Fixed some formatting issues, and added author field in posts

--- resources/views/heralds/index.blade.php
+++ resources/views/heralds/index.blade.php
@@ -6,17 +6,11 @@
 @guest
 <h1>Not logged in!</h1>
 @else
-@foreach ($heralds as $post)
-
 <div class="jumbotron">
-  <h1 class="display-3">{{ $post->title }}</h1>
-  <p class="lead">{{ $post->created_at }}</p>
-  <hr class="my-4">
-  <p>{{ $post->descr }}</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="/heralds/{{ $post->id }}" role="button">Read More</a>
-  </p>
-</div>
+<h1 class="display-3">Herald Index</h1>
+@foreach ($heralds as $post)
+    <a href="/heralds/{{ $post->id }}"><p class="lead">{{ $post->title }} at {{ $post->created_at }} by {{ $post->author }} </p></a>
 @endforeach
+</div>
 @endguest
 @endsection
--- resources/views/heralds/show.blade.php
+++ resources/views/heralds/show.blade.php
@@ -6,7 +6,7 @@
 @else
 <div class="jumbotron">
   <h1 class="display-3">{{ $herald->title }}</h1>
-  <p class="lead">{{ $herald->created_at }}</p>
+  <p class="lead">{{ $herald->created_at }} by {{ $herald->author }}</p>
   <hr class="my-4">
   <p>{{ $herald->herald }}</p>
 </div>
