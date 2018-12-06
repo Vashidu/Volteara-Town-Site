1e053bbdc715667ec4cea41dfcd7431386250804
Daniel <Vashidu@users.noreply.github.com>
2018-12-06 14:29:58 -0500

    Update title.blade.php

--- resources/views/title.blade.php
+++ resources/views/title.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="jumbotron">
   <h1 class="display-3">Town of Volteara</h1>
-  <p class="lead">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+  <p class="lead">Volteara is a Township under the county of Aronheim, which is in the Stormlands Duchy, within the Vornair Kingdom</p>
   <hr class="my-4">
-  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+  <p>To access the site please click the link below:</p>
   <p class="lead">
-    <a class="btn btn-primary btn-lg" href="/register" role="button">Register to learn more</a>
+    <a class="btn btn-primary btn-lg" href="/register" role="button">Register.</a>
   </p>
 </div>
 @endsection
